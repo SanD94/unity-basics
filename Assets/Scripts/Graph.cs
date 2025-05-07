624d70c54ef039690c6047f2b657477b2e1c24e3
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-05-07 12:01:00 +0200

    add time function

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.Video;
 
@@ -8,6 +9,8 @@ public class Graph : MonoBehaviour
     
     [SerializeField, Range(10, 100)]
     int resolution = 10;
+    
+    Transform[] points;
 
     void Awake()
     {
@@ -15,15 +18,26 @@ public class Graph : MonoBehaviour
         var position = Vector3.zero;
         var scale = Vector3.one * step;
 
-        for (int i = 0; i < resolution; i++) {
-            Transform point = Instantiate(pointPrefab);
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i] = Instantiate(pointPrefab);
             point.SetParent(transform, false);
 
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x*position.x;
             point.localPosition = position;
             point.localScale = scale;
         }
-        
+    }
+
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
+        }
     }
 }
