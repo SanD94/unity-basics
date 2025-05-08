b578f9babaa4509972b3d34df95d1f7b4fb397d9
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-05-08 13:02:59 +0200

    add GPU compute shader

---
+++ Assets/Scripts/GPUGraph.cs
@@ -0,0 +1,89 @@
+using System;
+using UnityEngine;
+
+public class GPUGraph : MonoBehaviour
+{
+    [SerializeField]
+    ComputeShader computeShader;
+    
+    static readonly int
+        positionsId = Shader.PropertyToID("_Positions"),
+        resolutionId = Shader.PropertyToID("_Resolution"),
+        stepId = Shader.PropertyToID("_Step"),
+        timeId = Shader.PropertyToID("_Time");
+    
+    [SerializeField, Range(10, 200)]
+    int resolution = 10;
+    
+    [SerializeField]
+    FunctionLibrary.FunctionName function;
+    
+    public enum TransitionMode { Cycle, Random }
+    
+    [SerializeField]
+    TransitionMode transitionMode;
+    
+    [SerializeField, Min(0f)]
+    float functionDuration = 1f, transitionDuration = 1f;
+    
+    float duration;
+    bool inTransition;
+    FunctionLibrary.FunctionName transitionFunction;
+
+    
+    ComputeBuffer positionsBuffer;
+    void OnEnable()
+    {
+        positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4); // Vector3
+    }
+
+    void OnDisable()
+    {
+        positionsBuffer.Release();
+        positionsBuffer = null;
+    }
+
+
+    void Update()
+    {
+        duration += Time.deltaTime;
+        if (inTransition)
+        {
+            if (duration >= functionDuration)
+            {
+                duration -= transitionDuration;
+                inTransition = false;
+            }
+
+        }
+        else if (duration >= functionDuration) 
+        {
+            duration -= functionDuration;
+            inTransition = true;
+            transitionFunction = function;
+            PickNextFunction();
+        }
+        
+        UpdateFunctionOnGPU();
+    }
+
+    void PickNextFunction()
+    {
+        function = transitionMode == TransitionMode.Cycle ?
+            FunctionLibrary.GetNextFunctionName(function) :
+            FunctionLibrary.GetRandomFunctionNameOtherThan(function);
+    }
+    
+    void UpdateFunctionOnGPU()
+    {
+        float step = 2f / resolution;
+        computeShader.SetInt(resolutionId, resolution);
+        computeShader.SetFloat(stepId, step);
+        computeShader.SetFloat(timeId, Time.time);
+        
+        computeShader.SetBuffer(0, positionsId, positionsBuffer);
+
+        int groups = Mathf.CeilToInt(resolution / 8f);
+        computeShader.Dispatch(0, groups, groups, 1);
+    }
+}
--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -1,7 +1,5 @@
 using System;
-using UnityEditor.Experimental.GraphView;
 using UnityEngine;
-using UnityEngine.Video;
 
 public class Graph : MonoBehaviour
 {
