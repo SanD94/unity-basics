3a04b89af762d5f6b6fcfcef4d047399647c9308
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-05-08 15:54:27 +0200

    add gpu instancing

--- Assets/Scripts/GPUGraph.cs
+++ Assets/Scripts/GPUGraph.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class GPUGraph : MonoBehaviour
@@ -12,7 +13,13 @@ public class GPUGraph : MonoBehaviour
         stepId = Shader.PropertyToID("_Step"),
         timeId = Shader.PropertyToID("_Time");
     
-    [SerializeField, Range(10, 200)]
+    [SerializeField]
+    Material material;
+
+    [SerializeField]
+    Mesh mesh;
+    
+    [SerializeField, Range(10, 1000)]
     int resolution = 10;
     
     [SerializeField]
@@ -85,5 +92,13 @@ public class GPUGraph : MonoBehaviour
 
         int groups = Mathf.CeilToInt(resolution / 8f);
         computeShader.Dispatch(0, groups, groups, 1);
+
+        material.SetBuffer(positionsId, positionsBuffer);
+		material.SetFloat(stepId, step);
+        
+        var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
+        Graphics.DrawMeshInstancedProcedural(
+            mesh, 0, material, bounds, positionsBuffer.count
+        );
     }
 }
