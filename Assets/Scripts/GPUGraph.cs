21b192a45ed522dc35ef5510b67cc9c96b0ff6c2
Safa Andac <8544658+SanD94@users.noreply.github.com>
2025-05-09 15:02:47 +0200

    add transitions

--- Assets/Scripts/GPUGraph.cs
+++ Assets/Scripts/GPUGraph.cs
@@ -10,7 +10,8 @@ public class GPUGraph : MonoBehaviour
         positionsId = Shader.PropertyToID("_Positions"),
         resolutionId = Shader.PropertyToID("_Resolution"),
         stepId = Shader.PropertyToID("_Step"),
-        timeId = Shader.PropertyToID("_Time");
+        timeId = Shader.PropertyToID("_Time"),
+        transitionProgressId = Shader.PropertyToID("_TransitionProgress");
     
     const int maxResolution = 1000;
     
@@ -89,7 +90,15 @@ public class GPUGraph : MonoBehaviour
         computeShader.SetFloat(stepId, step);
         computeShader.SetFloat(timeId, Time.time);
         
-        var kernelIndex = (int)function;
+        if (inTransition)
+        {
+            computeShader.SetFloat(
+                transitionProgressId,
+                Mathf.SmoothStep(0f, 1f, duration / transitionDuration)
+            );
+        }
+        
+        var kernelIndex = (int)function + (int)(inTransition ? transitionFunction : function) * 5;
         computeShader.SetBuffer(kernelIndex, positionsId, positionsBuffer);
 
         int groups = Mathf.CeilToInt(resolution / 8f);
